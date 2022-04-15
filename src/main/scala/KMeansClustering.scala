3bfd526d5ee8a066de1280b08a89aebb7d0c2dde
Francis Joseph <66641461+franckoj@users.noreply.github.com>
2022-04-15 13:49:28 +0530

    Create KMeansClustering.scala

---
+++ src/main/scala/KMeansClustering.scala
@@ -0,0 +1,37 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.clustering.KMeans
+
+object ClusteringKMeans extends App{
+
+  val spark = SparkSession.builder().appName("LinearRegression")
+    .master("local[*]")
+    .getOrCreate()
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  val path = "src/main/resources/Clustering/Wholesale customers data.csv"
+  val df = spark.read.option("inferSchema",true)
+    .option("header",true)
+    .csv(path)
+
+  val feature_data = df.select("Fresh", "Milk","Grocery","Frozen","Detergents_Paper","Delicassen")
+
+  val assembler = new VectorAssembler()
+    .setInputCols(feature_data.columns)
+    .setOutputCol("features")
+
+  val training_data = assembler.transform(df)
+    .select("features")
+
+  val kMeans = new KMeans()
+    .setK(2)
+
+  val model = kMeans.fit(training_data)
+  val modelSummary = model.summary
+
+
+  println(s"training cost - ${modelSummary.trainingCost}")
+  
+}
